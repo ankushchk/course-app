69a20d791fe81efa9a8a603481742abf79ab9c7e
Ankush Chauhan <ac.ankushchauhan.2006@gmail.com>
2024-07-24 17:06:35 +0530

    add

--- server/dist/index.js
+++ server/dist/index.js
@@ -9,7 +9,7 @@ const admin_1 = __importDefault(require("./routes/admin"));
 const user_1 = __importDefault(require("./routes/user"));
 const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
-const port = 3000;
+const port = "3000";
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.use("/admin", admin_1.default);
